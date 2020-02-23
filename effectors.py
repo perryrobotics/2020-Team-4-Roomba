aa26d03267abea111a8f870e01603169642bac6a
perryrobotics <47753315+perryrobotics@users.noreply.github.com>
2020-02-23 10:35:56 -0600

    Add files via upload

--- effectors.py
+++ effectors.py
@@ -5,13 +5,16 @@ KIPR=ctypes.CDLL("/usr/lib/libkipr.so")
 
 
 ARM_PORT = 0
-ARM_D = 0
+ARM_DOWN = 1640
 ARM_BACK = 2047
-ARM_BOT= 110
+ARM_BOT= 25
+ARM_BALL_UP= 0 
+ARM_BALL= 1800
 
 CLAW_PORT = 0
 CLAW_TICKS = 7000
 CLAW_TICKS_BOT = 15000
+CLAW_TICKS_BALL = CLAW_TICKS/1.4
     
 def move_servo_slow(port, current_pos, end_pos,step):
 	if end_pos < current_pos:
@@ -21,12 +24,17 @@ def move_servo_slow(port, current_pos, end_pos,step):
   		KIPR.msleep(40)
 	KIPR.set_servo_position(port,end_pos)
             
+def arm_ball(step):
+	move_servo_slow(ARM_PORT, KIPR.get_servo_position(ARM_PORT), ARM_BALL, step)     
+
+def arm_ball_up(step):
+	move_servo_slow(ARM_PORT, KIPR.get_servo_position(ARM_PORT), ARM_BALL_UP, step)    
             
 def arm_bot(step):
 	move_servo_slow(ARM_PORT, KIPR.get_servo_position(ARM_PORT), ARM_BOT, step)
         
 def arm_down(step):
-	move_servo_slow(ARM_PORT, KIPR.get_servo_position(ARM_PORT), ARM_D, step)
+	move_servo_slow(ARM_PORT, KIPR.get_servo_position(ARM_PORT), ARM_DOWN, step)
         
 def arm_back(step):
 	move_servo_slow(ARM_PORT, KIPR.get_servo_position(ARM_PORT), ARM_BACK, step)
@@ -51,7 +59,20 @@ def close_claw_bot(speed):
 	while KIPR.gmpc(CLAW_PORT) > -CLAW_TICKS_BOT:
 		pass
 	KIPR.ao()
-        
+  
+def close_claw_ball(speed):
+	KIPR.cmpc(CLAW_PORT)
+	KIPR.mav(CLAW_PORT, -1200)
+	while KIPR.gmpc(CLAW_PORT) > -CLAW_TICKS_BALL:
+		pass
+	KIPR.ao()
+            
+def open_claw_ball(speed):
+	KIPR.cmpc(CLAW_PORT)
+	KIPR.mav(CLAW_PORT, 1200)
+	while KIPR.gmpc(CLAW_PORT) < CLAW_TICKS_BALL:
+		pass
+	KIPR.ao()
         
         
         
--- main.py
+++ main.py
@@ -14,17 +14,38 @@ def main():
 		pass
 	print("Starting in 3 seconds!!!")
 	KIPR.msleep(3000)
-	arm_bot(7)
-	#open_claw(1000)
+	arm_bot(50) #joe
 	drive_backwards(250, 845)
-	close_claw_bot(1160)
+	close_claw_bot(1160) #grabthatbotty
 	drive_straight(250, 400)
 	spin_right(100, 80)
-	open_claw(1000)
-	arm_back(10)
-	spin_left(100,245)
-	
+	open_claw(1000) #dropit
+	for x in range(5):
+		spin_left(50,5)
+		spin_right(50,5)
             
+	arm_down(25) #allhailthegreatyarlie
+	spin_left(100,285)
+	drive_straight(250,172)
+	spin_right(100, 49)
+	drive_backwards(250,292) #throwitback and maybe more 
+	spin_left(100, 93)
+	drive_backwards(250, 518)
+	KIPR.msleep(30000) #stop
+	drive_straight(250, 525) #missonboll
+	spin_left(100, 95)
+	arm_ball(9) 
+	drive_straight(150,83)
+	close_claw_ball(956) #gropetheboll
+	arm_ball_up(25)
+	drive_backwards (250, 400) 
+	spin_left(100, 90) #todemountain
+	drive_backwards (250, 80) 
+	spin_right (100, 80) #ballyeet
+	spin_left(100, 45)
+	drive_backwards (250, 75) #originally110 but too far
+	open_claw_ball(1000) #ballenlightenment on the mountaintop 
+	KIPR.disable_servos()
 	
             
 	KIPR.create_disconnect()
